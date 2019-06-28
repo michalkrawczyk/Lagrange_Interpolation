ac648c2d90f00fae45185e00238e2edc0527ff8c
michalkrawczyk <235325@student.pwr.edu.pl>
2019-06-28 10:41:18 +0200

    Add files via upload

---
+++ Lagrange.m
@@ -0,0 +1,23 @@
+function [yEstimated] = Lagrange(XMatrix,YMatrix, xEstimate)
+
+nom=ones(1,length(XMatrix));
+denom=ones(1,length(XMatrix));
+
+for i=1:length(XMatrix)
+    for j=1:length(XMatrix)
+        if i~=j
+            nom(i)=nom(i)*(xEstimate-XMatrix(j));
+            denom(i)=denom(i)*(XMatrix(i)-XMatrix(j));
+        end
+    end
+end
+
+
+%L=nom./denom
+%L=YMatrix.*L
+L=YMatrix.*(nom./denom);
+yEstimated=sum(L);
+
+
+end
+
